68ddb1104d345594dd0489280501f6dfbd182c02
rahadiansetya <rahadiansetya@gmail.com>
2022-09-13 15:51:52 +0700

    melakukan perubahan text dan warna

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Hack</h2>
+          <h2>Welcome to Hackacgtive Vol 2</h2>
         </div>
         <p className="App-intro">
-          I have changed this text from feature branch 
+          This is the page you're edited before lunch
         </p>
       </div>
     );
